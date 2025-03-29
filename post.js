07bf05f522ee361acad576da3b901794b2160313
happy_shrimp <89066728+shrimpstack@users.noreply.github.com>
2025-03-29 21:28:17 +0800

    Update post.js

--- post.js
+++ post.js
@@ -1,4 +1,4 @@
-function post(url, data) {
+function post_1(url, data) {
   if(!window.XMLHttpRequest) {
     alert('無法連線，請更換瀏覽器');
     return;
@@ -21,5 +21,66 @@ function post(url, data) {
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     let content = JSON.stringify(data);
     xhr.send("content=" + encodeURI(content));
+  });
+}
+
+function post_2(url, param = {}) {
+  if(!window.XMLHttpRequest) {
+    alert('無法連線，請更換瀏覽器');
+    return;
+  }
+  if(!url) {
+    alert('未設定url');
+    return;
+  }
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.responseType = "json";
+    xhr.addEventListener("load", () => {
+      if(xhr.status == 200 || xhr.status == 201) {
+        resolve(xhr.response);
+      }
+      else {
+        reject(new Error(xhr.status));
+      }
+    });
+    xhr.addEventListener("error", () => {
+      console.log(xhr.response);
+    });
+    let formData = new FormData();
+    Object.entries(param).forEach(([key, val]) => {
+      formData.append(key, val);
+    });
+    xhr.send(formData);
+  });
+}
+
+function post_3(url, body = {}) {
+  if(!window.XMLHttpRequest) {
+    alert('無法連線，請更換瀏覽器');
+    return;
+  }
+  if(!url) {
+    alert('未設定url');
+    return;
+  }
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.responseType = "json";
+    xhr.addEventListener("load", () => {
+      if(xhr.status == 200 || xhr.status == 201) {
+        resolve(xhr.response);
+      }
+      else {
+        reject(new Error(xhr.status));
+      }
+    });
+    xhr.addEventListener("error", () => {
+      console.log(xhr.response);
+    });
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.send(JSON.stringify(body));
   });
 }
